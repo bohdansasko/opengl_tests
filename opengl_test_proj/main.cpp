0ee0d77cf2ac7acae93458f8c5d80d3c3d5651e9
TQOS <0000xffff@gmail.com>
2017-12-09 17:56:47 +0200

    OpenGL Basing: - add next libraries: glad, glfw - create simple instance of window that will be closed after pressed key "space"

--- opengl_test_proj/main.cpp
+++ opengl_test_proj/main.cpp
@@ -7,11 +7,69 @@
 //
 
 #include <iostream>
+#include "glad/glad.h"
+#include "GLFW/glfw3.h"
 
 using namespace std;
 
-int main(int argc, const char * argv[]) {
-    cout << "Hello, World!" << endl;
+struct Size {
+    int width;
+    int height;
+};
 
-    return 0;
+Size windowSize { 1136, 640 };
+
+enum MAIN_FUNC_STATUS {
+    SUCCESS = 0,
+    FAIL = -1
+};
+
+void windowResizeCallback(GLFWwindow* window, int width, int height);
+void processInput(GLFWwindow* window);
+
+int main(int argc, const char * argv[]) {
+    glfwInit();
+
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+
+    GLFWwindow* mainWindowPtr = glfwCreateWindow(windowSize.width, windowSize.height, "OpenGL Game", nullptr, nullptr);
+    if (!mainWindowPtr) {
+        cout << "Failed to create GL Window" << endl;
+        glfwTerminate();
+        return MAIN_FUNC_STATUS::FAIL;
+    }
+    glfwMakeContextCurrent(mainWindowPtr);
+
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+        cout << "Failed to initialize GLAD" << endl;
+        return MAIN_FUNC_STATUS ::FAIL;
+    }
+
+    glViewport(0, 0, windowSize.width, windowSize.height);
+    glfwSetFramebufferSizeCallback(mainWindowPtr, windowResizeCallback);
+
+
+    while(!glfwWindowShouldClose(mainWindowPtr)) {
+        processInput(mainWindowPtr);
+
+        glfwSwapBuffers(mainWindowPtr);
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+
+    return MAIN_FUNC_STATUS::SUCCESS;
+}
+
+void windowResizeCallback(GLFWwindow* window, int width, int height) {
+    glViewport(0, 0, width, height);
+}
+
+void processInput(GLFWwindow* window) {
+    if (glfwGetKey(window, GLFW_KEY_SPACE) == GLFW_PRESS) {
+        glfwSetWindowShouldClose(window, true);
+    }
 }
