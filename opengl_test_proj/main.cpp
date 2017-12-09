cadb7d99d46eab5909a0b2790234781acbb3002e
TQOS <0000xffff@gmail.com>
2017-12-09 14:39:22 +0200

    initial commit

---
+++ opengl_test_proj/main.cpp
@@ -0,0 +1,17 @@
+//
+//  main.cpp
+//  opengl_test_proj
+//
+//  Created by TQOS on 09.12.17.
+//  Copyright © 2017 TQOS. All rights reserved.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, const char * argv[]) {
+    cout << "Hello, World!" << endl;
+
+    return 0;
+}
