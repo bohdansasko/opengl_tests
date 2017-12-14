db21ba1122a026072a78f7e1de725f9c43c12de7
TQOS <0000xffff@gmail.com>
2017-12-14 23:42:54 +0200

    OpenGL Basing: - draw rectrangle using triangles and EBO(element buffer object)

--- sources/main.cpp
+++ sources/main.cpp
@@ -26,9 +26,15 @@ enum MAIN_FUNC_STATUS {
 };
 
 float vertices[] = {
-    -0.5f, -0.5f, 0.0f, // left
-     0.5f, -0.5f, 0.0f, // right
-     0.0f,  0.5f, 0.0f  // top
+    -0.5f,  0.5f, 0.0f, // top left
+    -0.5f, -0.5f, 0.0f, // bottom left
+     0.5f,  0.5f, 0.0f, // top right
+     0.5f, -0.5f, 0.0f, // bottom right
+};
+
+uint indices[] = {
+    0, 1, 3, // first triangle
+    1, 2, 3  // second triangle
 };
 
 void windowResizeCallback(GLFWwindow* window, int width, int height);
@@ -59,15 +65,19 @@ int main(int argc, const char * argv[]) {
         return MAIN_FUNC_STATUS ::FAIL;
     }
 
-    uint vaoUniqueId1, vboUniqueId1;
+    uint vaoUniqueId1, vboUniqueId1, eboUniqueId1;
 
     glGenBuffers(1, &vboUniqueId1);
+    glGenBuffers(1, &eboUniqueId1);
     glGenVertexArrays(1, &vaoUniqueId1);
 
     glBindVertexArray(vaoUniqueId1);
 
     glBindBuffer(GL_ARRAY_BUFFER, vboUniqueId1);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, eboUniqueId1);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
     glEnableVertexAttribArray(0);
@@ -87,7 +97,7 @@ int main(int argc, const char * argv[]) {
 
         glUseProgram(shaderProgramId);
         glBindVertexArray(vaoUniqueId1);
-        glDrawArrays(GL_TRIANGLES, 0, 3);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 
         glfwSwapBuffers(mainWindowPtr);
         glfwPollEvents();
